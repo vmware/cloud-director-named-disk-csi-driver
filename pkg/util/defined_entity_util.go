1253bcf7fe923c6b7ba9ae63f1af643bf139ef09
Anirudh9794 <aniruddha.9794@gmail.com>
2022-03-16 15:20:24 -0700

    Compare only vendor and nss while determining entity type of RDE (#35)
    * Compare only vendor and nss while determining entity type of RDE
    
    Signed-off-by: Aniruddha Shamasundar <aniruddha.9794@gmail.com>

--- pkg/util/defined_entity_util.go
+++ pkg/util/defined_entity_util.go
@@ -3,23 +3,34 @@ package util
 import (
 	 "fmt"
 	swaggerClient "github.com/vmware/cloud-director-named-disk-csi-driver/pkg/vcdswaggerclient"
+	 "strings"
 )
 
 const (
-	EntityTypePrefix = "urn:vcloud:type"
 	CAPVCDEntityTypeVendor = "vmware"
 	CAPVCDEntityTypeNss = "capvcdCluster"
-	CAPVCDEntityTypeVersion = "1.0.0"
 
 	NativeClusterEntityTypeVendor = "cse"
 	NativeClusterEntityTypeNss = "nativeCluster"
-	NativeClusterEntityTypeVersion = "2.0.0"
 )
 
-var (
-	CAPVCDEntityTypeID = fmt.Sprintf("%s:%s:%s:%s", EntityTypePrefix, CAPVCDEntityTypeVendor, CAPVCDEntityTypeNss, CAPVCDEntityTypeVersion)
-	NativeEntityTypeID = fmt.Sprintf("%s:%s:%s:%s", EntityTypePrefix, NativeClusterEntityTypeVendor, NativeClusterEntityTypeNss, NativeClusterEntityTypeVersion)
-)
+func isCAPVCDEntityType(entityTypeID string) bool {
+	entityTypeIDSplit := strings.Split(entityTypeID, ":")
+	// format is urn:vcloud:type:<vendor>:<nss>:<version>
+	if len(entityTypeIDSplit) != 6 {
+		return false
+	}
+	return entityTypeIDSplit[3] == CAPVCDEntityTypeVendor && entityTypeIDSplit[4] == CAPVCDEntityTypeNss
+}
+
+func isNativeClusterEntityType(entityTypeID string) bool {
+	entityTypeIDSplit := strings.Split(entityTypeID, ":")
+	// format is urn:vcloud:type:<vendor>:<nss>:<version>
+	if len(entityTypeIDSplit) != 6 {
+		return false
+	}
+	return entityTypeIDSplit[3] == NativeClusterEntityTypeVendor && entityTypeIDSplit[4] == NativeClusterEntityTypeNss
+}
 
 func GetPVsFromRDE(rde *swaggerClient.DefinedEntity) ([]string, error) {
 	statusEntry, ok := rde.Entity["status"]
@@ -32,9 +43,9 @@ func GetPVsFromRDE(rde *swaggerClient.DefinedEntity) ([]string, error) {
 	}
 
 	var pvInterfaces interface{}
-	if rde.EntityType == CAPVCDEntityTypeID {
+	if isCAPVCDEntityType(rde.EntityType) {
 		pvInterfaces = statusMap["persistentVolumes"]
-	} else if rde.EntityType == NativeEntityTypeID {
+	} else if isNativeClusterEntityType(rde.EntityType) {
 		pvInterfaces = statusMap["persistentVolumes"]
 	} else {
 		return nil, fmt.Errorf("entity type %s not supported by CSI", rde.EntityType)
@@ -68,9 +79,9 @@ func ReplacePVsInRDE(rde *swaggerClient.DefinedEntity, updatedPvs []string) (*sw
 		return nil, fmt.Errorf("unable to convert [%T] to map", statusEntry)
 	}
 
-	if rde.EntityType == CAPVCDEntityTypeID {
+	if isCAPVCDEntityType(rde.EntityType) {
 		statusMap["persistentVolumes"] = updatedPvs
-	} else if rde.EntityType == NativeEntityTypeID {
+	} else if isNativeClusterEntityType(rde.EntityType) {
 		statusMap["persistentVolumes"] = updatedPvs
 	} else {
 		return nil, fmt.Errorf("entity type %s not supported by CSI", rde.EntityType)
