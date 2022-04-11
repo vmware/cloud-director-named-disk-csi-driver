f6bfbcb7bb013228af11b34d84458a7e726983c7
ymo24 <102765549+ymo24@users.noreply.github.com>
2022-04-11 09:02:03 -0700

    fix the unit test failing issue #VCDA 3546 (#42)
    * fix the unit test failing issue #VCDA 3546
    
    * remove unused part
    
    Signed-off-by: ymo24 <ymo@vmware.com>
    
    * make variable 'vcdCsiClient' more readable
    
    Signed-off-by: ymo24 <ymo@vmware.com>

---
+++ pkg/vcdcsiclient/common_system_test.go
@@ -0,0 +1,105 @@
+/*
+   Copyright 2021 VMware, Inc.
+   SPDX-License-Identifier: Apache-2.0
+*/
+
+package vcdcsiclient
+
+import (
+	"fmt"
+	"github.com/vmware/cloud-provider-for-cloud-director/pkg/config"
+	"github.com/vmware/cloud-provider-for-cloud-director/pkg/vcdclient"
+	"os"
+	"path/filepath"
+)
+
+var (
+	gitRoot string = ""
+)
+
+func init() {
+	gitRoot = os.Getenv("GITROOT")
+	if gitRoot == "" {
+		// It is okay to panic here as this will be caught during dev
+		panic("GITROOT should be set")
+	}
+}
+
+func getStrValStrict(val interface{}, defaultVal string) string {
+	if strVal, ok := val.(string); ok {
+		return strVal
+	}
+
+	return defaultVal
+}
+
+func getBoolValStrict(val interface{}, defaultVal bool) bool {
+	if boolVal, ok := val.(bool); ok {
+		return boolVal
+	}
+
+	return defaultVal
+}
+
+func getTestVCDClient(inputMap map[string]interface{}) (*vcdclient.Client, error) {
+
+	testConfigFilePath := filepath.Join(gitRoot, "testdata/config_test.yaml")
+	configReader, err := os.Open(testConfigFilePath)
+	if err != nil {
+		return nil, fmt.Errorf("Unable to open file [%s]: [%v]", testConfigFilePath, err)
+	}
+	defer configReader.Close()
+
+	cloudConfig, err := config.ParseCloudConfig(configReader)
+	if err != nil {
+		return nil, fmt.Errorf("Unable to parse cloud config file [%s]: [%v]", testConfigFilePath, err)
+	}
+
+	insecure := true
+	cloudConfig.VCD.RefreshToken = "cUb5vlHzTJvkki47yIAI8qH1JWXI29ek"
+	getVdcClient := false
+	if inputMap != nil {
+		for key, val := range inputMap {
+			switch key {
+			case "host":
+				cloudConfig.VCD.Host = getStrValStrict(val, cloudConfig.VCD.Host)
+			case "org":
+				cloudConfig.VCD.Org = getStrValStrict(val, cloudConfig.VCD.Org)
+			case "user":
+				cloudConfig.VCD.User = getStrValStrict(val, cloudConfig.VCD.User)
+			case "secret":
+				cloudConfig.VCD.Secret = getStrValStrict(val, cloudConfig.VCD.Secret)
+			case "insecure":
+				insecure = getBoolValStrict(val, true)
+			case "clusterID":
+				cloudConfig.ClusterID = getStrValStrict(val, cloudConfig.ClusterID)
+			case "getVdcClient":
+				getVdcClient = getBoolValStrict(val, false)
+			case "refreshToken":
+				cloudConfig.VCD.RefreshToken = getStrValStrict(val, cloudConfig.VCD.RefreshToken)
+			case "userOrg":
+				cloudConfig.VCD.UserOrg = getStrValStrict(val, cloudConfig.VCD.UserOrg)
+			}
+		}
+	}
+
+	return vcdclient.NewVCDClientFromSecrets(
+		cloudConfig.VCD.Host,
+		cloudConfig.VCD.Org,
+		cloudConfig.VCD.VDC,
+		cloudConfig.VCD.VAppName,
+		"",
+		"",
+		cloudConfig.VCD.UserOrg,
+		cloudConfig.VCD.User,
+		cloudConfig.VCD.Secret,
+		cloudConfig.VCD.RefreshToken,
+		insecure,
+		cloudConfig.ClusterID,
+		nil,
+		0,
+		0,
+		"",
+		getVdcClient,
+	)
+}
--- pkg/vcdcsiclient/disks_system_test.go
+++ pkg/vcdcsiclient/disks_system_test.go
@@ -1,14 +1,12 @@
 /*
-    Copyright 2021 VMware, Inc.
-    SPDX-License-Identifier: Apache-2.0
+   Copyright 2021 VMware, Inc.
+   SPDX-License-Identifier: Apache-2.0
 */
-
 
 package vcdcsiclient
 
 import (
 	"fmt"
-	"github.com/vmware/cloud-provider-for-cloud-director/pkg/vcdclient"
 	"net/http"
 	"testing"
 
@@ -23,86 +21,87 @@ func foundStringInSlice(find string, slice []string) bool {
 			return true
 		}
 	}
-	return  false
+	return false
 }
 
 func TestDiskCreateAttach(t *testing.T) {
 
+	vcdCsiClient := new(Client)
+
 	// get client
-	vcdClient, err := vcdclient.getTestVCDClient(map[string]interface{}{
+	vcdClient, err := getTestVCDClient(map[string]interface{}{
 		"getVdcClient": true,
 	})
+	vcdCsiClient.VCDClient = vcdClient
 	assert.NoError(t, err, "Unable to get VCD client")
 	require.NotNil(t, vcdClient, "VCD Client should not be nil")
 
-	_, err = vcdClient.vdc.FindStorageProfileReference("dev")
+	_, err = vcdClient.VDC.FindStorageProfileReference("dev")
 	assert.Errorf(t, err, "unable to find storage profile reference")
 
-	_, err = vcdClient.vdc.FindStorageProfileReference("*")
+	_, err = vcdClient.VDC.FindStorageProfileReference("*")
 	assert.NoErrorf(t, err, "unable to find storage profile reference")
 
 	// create disk with bad storage profile: should not succeed
 	diskName := fmt.Sprintf("test-pvc-%s", uuid.New().String())
-	disk, err := vcdClient.CreateDisk(diskName, 100, VCDBusTypeSCSI, VCDBusSubTypeVirtualSCSI,
+	disk, err := vcdCsiClient.CreateDisk(diskName, 100, VCDBusTypeSCSI, VCDBusSubTypeVirtualSCSI,
 		"", "dev", true)
 	assert.Errorf(t, err, "should not be able to create disk with storage profile [dev]")
 	assert.Nil(t, disk, "disk created should be nil")
 
 	// create disk
 	diskName = fmt.Sprintf("test-pvc-%s", uuid.New().String())
-	disk, err = vcdClient.CreateDisk(diskName, 100, VCDBusTypeSCSI, VCDBusSubTypeVirtualSCSI,
+	disk, err = vcdCsiClient.CreateDisk(diskName, 100, VCDBusTypeSCSI, VCDBusSubTypeVirtualSCSI,
 		"", "*", true)
 	assert.NoErrorf(t, err, "unable to create disk with name [%s]", diskName)
 	require.NotNil(t, disk, "disk created should not be nil")
 	assert.NotNil(t, disk.UUID, "disk UUID should not be nil")
 
 	// try to create same disk with same parameters: should succeed
-	disk, err = vcdClient.CreateDisk(diskName, 100, VCDBusTypeSCSI, VCDBusSubTypeVirtualSCSI,
+	disk, err = vcdCsiClient.CreateDisk(diskName, 100, VCDBusTypeSCSI, VCDBusSubTypeVirtualSCSI,
 		"", "*", true)
 	assert.NoError(t, err, "unable to create disk again with name [%s]", diskName)
 	require.NotNil(t, disk, "disk created should not be nil")
 
 	// Check RDE was updated with PV
-	currRDEPvs, _, _, err := vcdClient.GetRDEPersistentVolumes()
+	currRDEPvs, _, _, err := vcdCsiClient.GetRDEPersistentVolumes()
 	assert.NoError(t, err, "unable to get RDE PVs after creating disk")
 	assert.Equal(t, true, foundStringInSlice(disk.Id, currRDEPvs), "Disk Id should be found in RDE")
 
-
 	// try to create same disk with different parameters; should not succeed
-	disk1, err := vcdClient.CreateDisk(diskName, 1000, VCDBusTypeSCSI, VCDBusSubTypeVirtualSCSI,
+	disk1, err := vcdCsiClient.CreateDisk(diskName, 1000, VCDBusTypeSCSI, VCDBusSubTypeVirtualSCSI,
 		"", "", true)
 	assert.Error(t, err, "should not be able to create same disk with different parameters")
 	assert.Nil(t, disk1, "disk should not be created")
 
-
-	// get VM
-	nodeID := "vm1"
+	// get VM nodeID should be the existing VM name
+	nodeID := "mstr-eb9x"
 	vm, err := vcdClient.FindVMByName(nodeID)
 	require.NoError(t, err, "unable to find VM [%s] by name", nodeID)
 	require.NotNil(t, vm, "vm should not be nil")
 
 	// attach to VM
-	err = vcdClient.AttachVolume(vm, disk)
+	err = vcdCsiClient.AttachVolume(vm, disk)
 	assert.NoError(t, err, "unable to attach disk [%s] to vm [%#v]", disk.Name, vm)
 
-	attachedVMs, err := vcdClient.govcdAttachedVM(disk)
+	attachedVMs, err := vcdCsiClient.govcdAttachedVM(disk)
 	assert.NoError(t, err, "unable to get VMs attached to disk [%#v]", disk)
 	assert.NotNil(t, attachedVMs, "VM [%s] should be returned", nodeID)
 	assert.EqualValues(t, len(attachedVMs), 1, "[%d] VM(s) should be returned", 1)
 	assert.EqualValues(t, attachedVMs[0].Name, nodeID, "VM Name should be [%s]", nodeID)
 
-	err = vcdClient.DetachVolume(vm, disk.Name)
+	err = vcdCsiClient.DetachVolume(vm, disk.Name)
 	assert.NoError(t, err, "unable to detach disk [%s] from vm [%#v]", disk.Name, vm)
 
-	attachedVMs, err = vcdClient.govcdAttachedVM(disk)
+	attachedVMs, err = vcdCsiClient.govcdAttachedVM(disk)
 	assert.NoError(t, err, "unable to get VMs attached to disk [%#v]", disk)
 	assert.Nil(t, attachedVMs, "no VM should be returned", nodeID)
 
-	err = vcdClient.DeleteDisk(diskName)
+	err = vcdCsiClient.DeleteDisk(diskName)
 	assert.NoError(t, err, "unable to delete disk [%s]", disk.Name)
 
 	// Check PV was removed from RDE
-	currRDEPvs, _, _, err = vcdClient.GetRDEPersistentVolumes()
+	currRDEPvs, _, _, err = vcdCsiClient.GetRDEPersistentVolumes()
 	assert.NoError(t, err, "unable to get RDE PVs after deleting disk")
 	assert.False(t, foundStringInSlice(disk.Id, currRDEPvs), "Disk Id should not be found in RDE")
 
@@ -111,16 +110,19 @@ func TestDiskCreateAttach(t *testing.T) {
 
 func TestRdeEtag(t *testing.T) {
 
+	vcdCsiClient := new(Client)
+
 	// get client
 	vcdClient, err := getTestVCDClient(map[string]interface{}{
 		"getVdcClient": true,
 	})
+	vcdCsiClient.VCDClient = vcdClient
 	assert.NoError(t, err, "Unable to get VCD client")
 	require.NotNil(t, vcdClient, "VCD Client should not be nil")
 
-	rdePvs1, etag1, defEnt1, err := vcdClient.GetRDEPersistentVolumes()
+	rdePvs1, etag1, defEnt1, err := vcdCsiClient.GetRDEPersistentVolumes()
 	assert.NoError(t, err, "unable to get RDE PVs")
-	rdePvs2, etag2, defEnt2, err := vcdClient.GetRDEPersistentVolumes()
+	rdePvs2, etag2, defEnt2, err := vcdCsiClient.GetRDEPersistentVolumes()
 	assert.NoError(t, err, "unable to get RDE PVs on second attempt")
 	assert.Equal(t, etag1, etag2, "etags from consecutive GETs should be equal")
 	origRdePvs := make([]string, len(rdePvs1))
@@ -130,36 +132,36 @@ func TestRdeEtag(t *testing.T) {
 	addPv1 := "pv1"
 	addPv2 := "pv2"
 	updatedRdePvs1 := append(rdePvs1, addPv1)
-	httpResponse1, err := vcdClient.updateRDEPersistentVolumes(updatedRdePvs1, etag1, defEnt1)
+	httpResponse1, err := vcdCsiClient.updateRDEPersistentVolumes(updatedRdePvs1, etag1, defEnt1)
 	assert.NoError(t, err, "should have no error when update RDE on first attempt")
 	assert.Equal(t, http.StatusOK, httpResponse1.StatusCode, "first RDE update should have an OK (200) response")
-	rdePvs3, _, _, err := vcdClient.GetRDEPersistentVolumes()
+	rdePvs3, _, _, err := vcdCsiClient.GetRDEPersistentVolumes()
 	assert.NoError(t, err, "unable to get RDE PVs")
 	assert.Equal(t, true, foundStringInSlice(addPv1, rdePvs3), "pv [%s] should be found in rde pvs", addPv1)
 
 	// try updating RDE PVs with outdated etag
 	updatedRdePvs2 := append(rdePvs2, addPv2)
-	httpResponse2, err := vcdClient.updateRDEPersistentVolumes(updatedRdePvs2, etag2, defEnt2)
+	httpResponse2, err := vcdCsiClient.updateRDEPersistentVolumes(updatedRdePvs2, etag2, defEnt2)
 	assert.Error(t, err, "updating RDE with outdated etag should have an error")
 	assert.Equal(t, http.StatusPreconditionFailed, httpResponse2.StatusCode, "updating RDE does not have precondition failed (412) status code")
-	rdePvs3, etag3, defEnt3, err := vcdClient.GetRDEPersistentVolumes()
+	rdePvs3, etag3, defEnt3, err := vcdCsiClient.GetRDEPersistentVolumes()
 	assert.NoError(t, err, "unable to get RDE PVs")
 	assert.False(t, foundStringInSlice(addPv2, rdePvs3), "pv [%s] should not be in rde pvs", addPv2)
 
 	// try updating RDE PVs with current etag
 	updatedRdePvs3 := append(rdePvs3, addPv2)
-	httpResponse3, err := vcdClient.updateRDEPersistentVolumes(updatedRdePvs3, etag3, defEnt3)
+	httpResponse3, err := vcdCsiClient.updateRDEPersistentVolumes(updatedRdePvs3, etag3, defEnt3)
 	assert.NoError(t, err, "should have no error updating RDE with current etag")
 	assert.Equal(t, http.StatusOK, httpResponse3.StatusCode, "updating PV had status code [%d] instead of 200 (OK)", httpResponse3.StatusCode)
-	rdePvs4, etag4, defEnt4, err := vcdClient.GetRDEPersistentVolumes()
+	rdePvs4, etag4, defEnt4, err := vcdCsiClient.GetRDEPersistentVolumes()
 	assert.NoError(t, err, "unable to get RDE PVs")
 	assert.Equal(t, true, foundStringInSlice(addPv2, rdePvs4), "pv [%s] should be found in RDE PVs", addPv2)
 
 	// clean up: reset RDE PVs
-	httpResponse5, err := vcdClient.updateRDEPersistentVolumes(origRdePvs, etag4, defEnt4)
+	httpResponse5, err := vcdCsiClient.updateRDEPersistentVolumes(origRdePvs, etag4, defEnt4)
 	assert.NoError(t, err, "should have no error updating RDE with current etag")
 	assert.Equal(t, http.StatusOK, httpResponse5.StatusCode, "updating PV had status code 200 (OK)")
-	rdePvs5, _, _, err := vcdClient.GetRDEPersistentVolumes()
+	rdePvs5, _, _, err := vcdCsiClient.GetRDEPersistentVolumes()
 	assert.NoError(t, err, "unable to get RDE PVs")
 	assert.False(t, foundStringInSlice(addPv1, rdePvs5), "pv [%s] should not be found in rde pvs", addPv1)
 	assert.False(t, foundStringInSlice(addPv2, rdePvs5), "pv [%s] should not be found in rde pvs", addPv2)
