e9719aaa1a95b9e9d41672a803154451e21301fb
ymo24 <102765549+ymo24@users.noreply.github.com>
2023-06-05 23:03:11 -0700

    [VKC-121] Fix static provisioning failure (#188)
    * change default time to long time
    
    Signed-off-by: ymo24 <ymo@vmware.com>
    
    * minor fix into static provisioning test
    
    Signed-off-by: ymo24 <ymo@vmware.com>

--- tests/e2e/dynamic_provisioning_test.go
+++ tests/e2e/dynamic_provisioning_test.go
@@ -43,7 +43,7 @@ var _ = Describe("CSI dynamic provisioning Test", func() {
 		OrgName:      org,
 		Username:     userName,
 		RefreshToken: refreshToken,
-		UserOrg:      "system",
+		UserOrg:      userOrg,
 		GetVdcClient: true,
 	}, rdeId)
 	Expect(err).NotTo(HaveOccurred())
